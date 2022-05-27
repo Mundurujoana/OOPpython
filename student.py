635516bffadcbf29edb6aabcf513d71461c0bd00
Mundurujoana <mundujoana@gmail.com>
2022-05-27 19:33:47 +0300

    first commit

--- student.py
+++ student.py
@@ -8,10 +8,13 @@ class Student:
         self.last_name=last_name
         self.age=age
         self.country=country
+        
  
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
         
+    def get_initial(self):
+        return f"{self.first_name[0].split()} {self.last_name[0].split()}"
 
     def greeting(self):
       return f"Hello {self.first_name} {self.last_name} from {self.country}, welcome to {self.school}"
